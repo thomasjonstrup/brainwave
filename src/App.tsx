5aa7d674cc7c44bf2ba0ac7f8ab1b351c3251428
Thomas Jonstrup <thomas_jonstrup@hotmail.com>
2024-03-09 13:34:35 +0100

    New components
    Added new components Benefits and Services

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import Roadmap from "@/components/Roadmap";
 import Footer from "@/components/Footer";
 import Pricing from "@/components/Pricing";
 import Collaboration from "@/components/Collaboration";
+import Services from "@/components/Services";
+import Benefits from "@/components/Benefits";
 
 import ButtonGradient from "@/assets/svg/ButtonGradient";
 
@@ -13,7 +15,9 @@ const App = () => {
 			<div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
 				<Header />
 				<Hero />
+				<Benefits />
 				<Collaboration />
+				<Services />
 				<Pricing />
 				<Roadmap />
 				<Footer />
